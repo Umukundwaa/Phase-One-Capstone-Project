14bad5e6ece956e7c533a81be7b33693d4c6ae4e
umukundwaa <a.umukundwa1@alustudent.com>
2026-02-23 08:23:47 -0800

    Implement flat-rate tuition calculation for UndergraduateStudent

--- src/university/model/UndergraduateStudent.java
+++ src/university/model/UndergraduateStudent.java
@@ -1,16 +1,26 @@
 package university.model;
 public class UndergraduateStudent extends Student {
+
+    private static final double FLAT_RATE = 3000.0;
+
     public UndergraduateStudent(int id, String name, String email) {
         super(id, name, email);
     }
 
+
     @Override
+
+    public double calculateTuition() {
+        return FLAT_RATE;
+    }
 
     public String toString() {
         return "UndergraduateStudent [" +
                 "id='" + getId() + '\'' +
                 ", name='" + getName() + '\'' +
                 ", email='" + getEmail() + '\'' +
+                ", totalCredits=" + getTotalCredits() +
+                ", tuition= $ " + calculateTuition() +
                 ']';
     }
 
