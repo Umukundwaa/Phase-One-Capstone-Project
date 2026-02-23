523291cd5dcb2c3ecf3f956ed7b1177e21761bba
umukundwaa <a.umukundwa1@alustudent.com>
2026-02-23 09:44:07 -0800

    add Student Already EnrolledException file

---
+++ src/university/exceptions/StudentAlreadyEnrolledException.java
@@ -0,0 +1,8 @@
+package university.exceptions;
+
+public class StudentAlreadyEnrolledException extends Exception {
+
+    public StudentAlreadyEnrolledException(String message) {
+        super(message);
+    }
+}
