0f7bb4f633d6956c54e039758fc9a6c788129836
umukundwaa <a.umukundwa1@alustudent.com>
2026-02-25 08:45:09 -0800

    Implement Instructor class

--- src/university/model/Instructor.java
+++ src/university/model/Instructor.java
@@ -5,44 +5,35 @@ import java.util.List;
 
 public class Instructor extends Person {
     private String department;
+    private List<Course> assignedCourses;
 
-    List<Course> assignedCourses;
-
-
-    public Instructor(int id, String name, String email, String department){
-        super( id, name, email);
+    public Instructor(int id, String name, String email, String department) {
+        super(id, name, email);
         this.department = department;
         this.assignedCourses = new ArrayList<>();
     }
-    public String getDepartment() {
-        return department;
-    }
+
+    public String getDepartment() { return department; }
+
     public void assignCourse(Course course) {
         if (!assignedCourses.contains(course)) {
             assignedCourses.add(course);
         }
     }
-    public List<Course> getAssignedCourses(){
-        return assignedCourses;
-    }
 
-public void assignGrade(Student student, Course course, double grade){
+    public List<Course> getAssignedCourses() { return assignedCourses; }
+
+    public void assignGrade(Student student, Course course, double grade) {
         if (assignedCourses.contains(course)) {
             student.assignGrade(course, grade);
-        }
-        
-        else{
+        } else {
             throw new IllegalArgumentException("Instructor is not assigned to this course.");
         }
     }
 
     @Override
     public String toString() {
-        return "Instructor [" +
-                "id='" + getId() + '\'' +
-                ", name='" + getName() + '\'' +
-                ", email='" + getEmail() + '\'' +
-                ", department='" + department + '\'' +
-                ']';
+        return "Instructor [id=" + getId() + ", name='" + getName() + "', email='" + getEmail() +
+                "', department='" + department + "']";
     }
 }
