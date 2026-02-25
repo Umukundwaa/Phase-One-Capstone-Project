0bfcc872c8846e01829755b90b9a0c727fdb9c9c
umukundwaa <a.umukundwa1@alustudent.com>
2026-02-25 08:42:42 -0800

    Implement Person class

--- src/university/model/Person.java
+++ src/university/model/Person.java
@@ -5,29 +5,18 @@ public abstract class Person {
     private String name;
     private String email;
 
-
-    public Person(int id , String name , String email) {
+    public Person(int id, String name, String email) {
         this.id = id;
         this.name = name;
         this.email = email;
     }
-    public int getId() {
-        return id;
-    }
-    public String getName() {
-        return name;
-    }
-    public String getEmail() {
-        return email;
-    }
+
+    public int getId() { return id; }
+    public String getName() { return name; }
+    public String getEmail() { return email; }
 
     @Override
     public String toString() {
-        return "Person [" +
-                "id='" + id + '\'' +
-                ", name='" + name + '\'' +
-                ", email='" + email + '\'' +
-                ']';
+        return "Person [id='" + id + "', name='" + name + "', email='" + email + "']";
     }
-
 }
